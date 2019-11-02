8200cf7706f929920223b678542bdf0c1abdcc7f
bold <bold@cryptoguru.com>
2019-11-02 05:50:11 -0700

    integrate modules into basic teleoperation loop

--- examples/GLFW/32-spring/32-spring.cpp
+++ examples/GLFW/32-spring/32-spring.cpp
@@ -1,5 +1,10 @@
 #include "chai3d.h"
 #include <GLFW/glfw3.h>
+#include "Network.h"
+#include "Spring.h"
+#include "Master.h"
+#include "Slave.h"
+#include "chrono"
 
 using namespace chai3d;
 using namespace std;
@@ -34,7 +39,7 @@ cWorld* world;
 cCamera* camera;
 
 // a light source to illuminate the objects in the world
-cDirectionalLight *light;
+cDirectionalLight* light;
 
 // a haptic device handler
 cHapticDeviceHandler* handler;
@@ -55,13 +60,22 @@ cThread* hapticsThread;
 GLFWwindow* window = nullptr;
 
 // a spring the haptic device interacts with
-cMesh* spring;
+Spring* spring;
+
+// the network model used for passing messages between master and slave
+Network* network;
 
 // a wall that fixes the spring
 cMesh* wall;
 
+// master environment interacting with a haptic device
+Master* master;
+
+// remote environment controlled by the master
+Slave* slave;
+
 // current width of window
-int width  = 0;
+int width = 0;
 
 // current height of window
 int height = 0;
@@ -92,77 +106,77 @@ void updateHaptics();
 // this function closes the application
 void close();
 
-cMesh* createSpring(double size, double height);
+cMesh* createSpringMesh(double size, double height);
 
 cMesh* createWall();
 
 void initHapticDevice()
 {
 	handler = new cHapticDeviceHandler();
-    handler->getDevice(hapticDevice, 0);
-    hapticDevice->open();
-    hapticDevice->calibrate();
+	handler->getDevice(hapticDevice, 0);
+	hapticDevice->open();
+	hapticDevice->calibrate();
 }
 
 void initOpenGL()
 {
 	if (!glfwInit())
-    {
-        cout << "failed initialization" << endl;
-        cSleepMs(1000);
+	{
+		cout << "failed initialization" << endl;
+		cSleepMs(1000);
 		exit(1);
-    }
+	}
 
-    glfwSetErrorCallback(errorCallback);
+	glfwSetErrorCallback(errorCallback);
 
-    // compute desired size of window
+	// compute desired size of window
 	const auto mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    const int w = mode->height * 0.8;
-    const int h = mode->height * 0.5;
-    const int x = (mode->width - w) * 0.5;
-    const int y = (mode->height - h) * 0.5;
+	const int w = mode->height * 0.8;
+	const int h = mode->height * 0.5;
+	const int x = (mode->width - w) * 0.5;
+	const int y = (mode->height - h) * 0.5;
 
-    // set OpenGL version
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	// set OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
-    if (stereoMode == C_STEREO_ACTIVE)
-    {
-        glfwWindowHint(GLFW_STEREO, GL_TRUE);
-    }
-    else
-    {
-        glfwWindowHint(GLFW_STEREO, GL_FALSE);
-    }
+	if (stereoMode == C_STEREO_ACTIVE)
+	{
+		glfwWindowHint(GLFW_STEREO, GL_TRUE);
+	}
+	else
+	{
+		glfwWindowHint(GLFW_STEREO, GL_FALSE);
+	}
 
-    // create display context
-    window = glfwCreateWindow(w, h, "CHAI3D", NULL, NULL);
-    if (!window)
-    {
-        cout << "failed to create window" << endl;
-        cSleepMs(1000);
-        glfwTerminate();
+	// create display context
+	window = glfwCreateWindow(w, h, "CHAI3D", nullptr, nullptr);
+	if (!window)
+	{
+		cout << "failed to create window" << endl;
+		cSleepMs(1000);
+		glfwTerminate();
 		exit(1);
-    }
+	}
 
-    glfwGetWindowSize(window, &width, &height);
-    glfwSetWindowPos(window, x, y);
+	glfwGetWindowSize(window, &width, &height);
+	glfwSetWindowPos(window, x, y);
 
-    glfwSetKeyCallback(window, keyCallback);
+	glfwSetKeyCallback(window, keyCallback);
 
-    glfwSetWindowSizeCallback(window, windowSizeCallback);
+	glfwSetWindowSizeCallback(window, windowSizeCallback);
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-    glfwSwapInterval(swapInterval);
+	glfwSwapInterval(swapInterval);
 
 #ifdef GLEW_VERSION
-    if (glewInit() != GLEW_OK)
-    {
-        cout << "failed to initialize GLEW library" << endl;
-        glfwTerminate();
-        exit(1);
-    }
+	if (glewInit() != GLEW_OK)
+	{
+		cout << "failed to initialize GLEW library" << endl;
+		glfwTerminate();
+		exit(1);
+	}
 #endif
 }
 
@@ -172,32 +186,33 @@ void initWorld()
 
 	world->setBackgroundColor(0.5, 0.5, 0.5);
 
-    world->m_backgroundColor.setBlack();
+	world->m_backgroundColor.setBlack();
 
-    camera = new cCamera(world);
-    world->addChild(camera);
+	camera = new cCamera(world);
+	world->addChild(camera);
 
-    camera->set( cVector3d (0.5, 0.0, 0.0),    // camera position (eye)
-                 cVector3d (0.0, 0.0, 0.0),    // look at position (target)
-                 cVector3d (0.0, 0.0, 1.0));   // direction of the (up) vector
+	camera->set(cVector3d(0.5, 0.0, 0.0), // camera position (eye)
+	            cVector3d(0.0, 0.0, 0.0), // look at position (target)
+	            cVector3d(0.0, 0.0, 1.0)); // direction of the (up) vector
 
-    camera->setClippingPlanes(0.01, 10.0);
+	camera->setClippingPlanes(0.01, 10.0);
 
-    camera->setStereoMode(stereoMode);
+	camera->setStereoMode(stereoMode);
 
-    camera->setStereoEyeSeparation(0.01);
-    camera->setStereoFocalLength(0.5);
+	camera->setStereoEyeSeparation(0.01);
+	camera->setStereoFocalLength(0.5);
 
-    camera->setMirrorVertical(mirroredDisplay);
+	camera->setMirrorVertical(mirroredDisplay);
 
-    light = new cDirectionalLight(world);
-    world->addChild(light);
-    light->setEnabled(true);
+	light = new cDirectionalLight(world);
+	world->addChild(light);
+	light->setEnabled(true);
 
-    light->setDir(-1.0, 0.0, 0.0);
+	light->setDir(-1.0, 0.0, 0.0);
 
-	spring = createSpring(0.05, 0.085);
-    world->addChild(spring);
+	cMesh* springMesh = createSpringMesh(0.05, 0.085);
+	world->addChild(springMesh);
+	spring = new Spring(springMesh);
 
 	wall = createWall();
 	world->addChild(wall);
@@ -206,64 +221,70 @@ void initWorld()
 
 int main(int argc, char* argv[])
 {
-    cout << endl;
-    cout << "-----------------------------------" << endl;
-    cout << "CHAI3D" << endl;
-    cout << "Demo: 32-spring" << endl;
-    cout << "Copyright 2003-2016" << endl;
-    cout << "-----------------------------------" << endl << endl << endl;
-    cout << "Keyboard Options:" << endl << endl;
-    cout << "[1] - Enable/Disable potential field" << endl;
-    cout << "[2] - Enable/Disable damping" << endl;
-    cout << "[f] - Enable/Disable full screen mode" << endl;
-    cout << "[m] - Enable/Disable vertical mirroring" << endl;
-    cout << "[q] - Exit application" << endl;
-    cout << endl << endl;
+	cout << endl;
+	cout << "-----------------------------------" << endl;
+	cout << "CHAI3D" << endl;
+	cout << "Demo: 32-spring" << endl;
+	cout << "Copyright 2003-2016" << endl;
+	cout << "-----------------------------------" << endl << endl << endl;
+	cout << "Keyboard Options:" << endl << endl;
+	cout << "[1] - Enable/Disable potential field" << endl;
+	cout << "[2] - Enable/Disable damping" << endl;
+	cout << "[f] - Enable/Disable full screen mode" << endl;
+	cout << "[m] - Enable/Disable vertical mirroring" << endl;
+	cout << "[q] - Exit application" << endl;
+	cout << endl << endl;
 
 	initHapticDevice();
 
-    const auto info = hapticDevice->getSpecifications();
+	const auto info = hapticDevice->getSpecifications();
 
 	initOpenGL();
 
 	initWorld();
 
-    // create a thread which starts the main haptics rendering loop
-    hapticsThread = new cThread();
-    hapticsThread->start(updateHaptics, CTHREAD_PRIORITY_HAPTICS);
+	std::chrono::microseconds delay(0);
+	std::chrono::microseconds varDelay(0);
+	network = new Network(delay, varDelay);
+	master = new Master(network, hapticDevice);
+	slave = new Slave(network, spring);
 
-    atexit(close);
+	// create a thread which starts the main haptics rendering loop
+	hapticsThread = new cThread();
+	hapticsThread->start(updateHaptics, CTHREAD_PRIORITY_HAPTICS);
 
-    windowSizeCallback(window, width, height);
+	atexit(close);
 
-    // main graphic loop
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwGetWindowSize(window, &width, &height);
+	windowSizeCallback(window, width, height);
 
-        updateGraphics();
+	// main graphic loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwGetWindowSize(window, &width, &height);
 
-        glfwSwapBuffers(window);
+		updateGraphics();
 
-        glfwPollEvents();
-    }
+		glfwSwapBuffers(window);
 
-    glfwDestroyWindow(window);
+		glfwPollEvents();
+	}
 
-    glfwTerminate();
+	glfwDestroyWindow(window);
 
-    return 0;
+	glfwTerminate();
+
+	return 0;
 }
 
 void windowSizeCallback(GLFWwindow* window, const int newWidth, int a_height)
 {
-    width  = newWidth;
-    height = a_height;
+	width = newWidth;
+	height = a_height;
 }
 
 void errorCallback(int error, const char* description)
 {
-    cout << "Error: " << description << endl;
+	cout << "Error: " << description << endl;
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -272,81 +293,81 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
 void close(void)
 {
-    // stop the simulation
-    simulationRunning = false;
+	// stop the simulation
+	simulationRunning = false;
 
-    // wait for graphics and haptics loops to terminate
-    while (!simulationFinished) { cSleepMs(100); }
+	// wait for graphics and haptics loops to terminate
+	while (!simulationFinished) { cSleepMs(100); }
 
-    hapticDevice->close();
+	hapticDevice->close();
 
-    // delete resources
-    delete hapticsThread;
-    delete world;
-    delete handler;
+	// delete resources
+	delete hapticsThread;
+	delete world;
+	delete handler;
 }
 
-cMesh* createSpring(const double size, const double height)
+cMesh* createSpringMesh(const double size, const double height)
 {
-	auto spring = new cMesh();
+	auto mesh = new cMesh();
 	const auto base = 0.0;
 	const auto halfSize = size / 2.0;
 	int vertices[6][6];
 
 	// face -x
-	vertices[0][0] = spring->newVertex(-halfSize, base, halfSize);
-	vertices[0][1] = spring->newVertex(-halfSize, base, -halfSize);
-	vertices[0][2] = spring->newVertex(-halfSize, -height, -halfSize);
-	vertices[0][3] = spring->newVertex(-halfSize, -height, halfSize);
+	vertices[0][0] = mesh->newVertex(-halfSize, base, halfSize);
+	vertices[0][1] = mesh->newVertex(-halfSize, base, -halfSize);
+	vertices[0][2] = mesh->newVertex(-halfSize, -height, -halfSize);
+	vertices[0][3] = mesh->newVertex(-halfSize, -height, halfSize);
 
 	// face +x
-	vertices[1][0] = spring->newVertex(halfSize, base, -halfSize);
-	vertices[1][1] = spring->newVertex(halfSize, base, halfSize);
-	vertices[1][2] = spring->newVertex(halfSize, -height, halfSize);
-	vertices[1][3] = spring->newVertex(halfSize, -height, -halfSize);
+	vertices[1][0] = mesh->newVertex(halfSize, base, -halfSize);
+	vertices[1][1] = mesh->newVertex(halfSize, base, halfSize);
+	vertices[1][2] = mesh->newVertex(halfSize, -height, halfSize);
+	vertices[1][3] = mesh->newVertex(halfSize, -height, -halfSize);
 
 	// face -y
-	vertices[2][0] = spring->newVertex(-halfSize, -height, halfSize);
-	vertices[2][1] = spring->newVertex(-halfSize, -height, -halfSize);
-	vertices[2][2] = spring->newVertex(halfSize, -height, -halfSize);
-	vertices[2][3] = spring->newVertex(halfSize, -height, halfSize);
+	vertices[2][0] = mesh->newVertex(-halfSize, -height, halfSize);
+	vertices[2][1] = mesh->newVertex(-halfSize, -height, -halfSize);
+	vertices[2][2] = mesh->newVertex(halfSize, -height, -halfSize);
+	vertices[2][3] = mesh->newVertex(halfSize, -height, halfSize);
 
 	// face +y
-	vertices[3][0] = spring->newVertex(halfSize, base, halfSize);
-	vertices[3][1] = spring->newVertex(halfSize, base, -halfSize);
-	vertices[3][2] = spring->newVertex(-halfSize, base, halfSize);
-	vertices[3][3] = spring->newVertex(-halfSize, base, -halfSize);
+	vertices[3][0] = mesh->newVertex(halfSize, base, halfSize);
+	vertices[3][1] = mesh->newVertex(halfSize, base, -halfSize);
+	vertices[3][2] = mesh->newVertex(-halfSize, base, halfSize);
+	vertices[3][3] = mesh->newVertex(-halfSize, base, -halfSize);
 
 	// face -z
-	vertices[4][0] = spring->newVertex(halfSize, base, -halfSize);
-	vertices[4][1] = spring->newVertex(-halfSize, base, -halfSize);
-	vertices[4][2] = spring->newVertex(halfSize, -height, -halfSize);
-	vertices[4][3] = spring->newVertex(-halfSize, -height, -halfSize);
+	vertices[4][0] = mesh->newVertex(halfSize, base, -halfSize);
+	vertices[4][1] = mesh->newVertex(-halfSize, base, -halfSize);
+	vertices[4][2] = mesh->newVertex(halfSize, -height, -halfSize);
+	vertices[4][3] = mesh->newVertex(-halfSize, -height, -halfSize);
 
 	// face +z
-	vertices[5][0] = spring->newVertex(halfSize, base, halfSize);
-	vertices[5][1] = spring->newVertex(-halfSize, base, halfSize);
-	vertices[5][2] = spring->newVertex(halfSize, -height, halfSize);
-	vertices[5][3] = spring->newVertex(-halfSize, -height, halfSize);
+	vertices[5][0] = mesh->newVertex(halfSize, base, halfSize);
+	vertices[5][1] = mesh->newVertex(-halfSize, base, halfSize);
+	vertices[5][2] = mesh->newVertex(halfSize, -height, halfSize);
+	vertices[5][3] = mesh->newVertex(-halfSize, -height, halfSize);
 
 	// create triangles
 	for (auto& v : vertices)
 	{
-		spring->newTriangle(v[0], v[1], v[2]);
-		spring->newTriangle(v[0], v[2], v[3]);
+		mesh->newTriangle(v[0], v[1], v[2]);
+		mesh->newTriangle(v[0], v[2], v[3]);
 	}
-	
-	spring->m_material->m_ambient.set(0.5, 0.5, 0.5, 1.0);
-	spring->m_material->m_diffuse.set(0.7, 0.7, 0.7, 1.0);
-	spring->m_material->m_specular.set(1.0, 1.0, 1.0, 1.0);
-	spring->m_material->m_emission.set(0.0, 0.0, 0.0, 1.0);
-	spring->m_material->setStiffness(50);
-	spring->m_material->setDynamicFriction(0.8);
-	spring->m_material->setStaticFriction(0.8);
 
-	spring->setLocalPos(0, 0, 0);
+	mesh->m_material->m_ambient.set(0.5, 0.5, 0.5, 1.0);
+	mesh->m_material->m_diffuse.set(0.7, 0.7, 0.7, 1.0);
+	mesh->m_material->m_specular.set(1.0, 1.0, 1.0, 1.0);
+	mesh->m_material->m_emission.set(0.0, 0.0, 0.0, 1.0);
+	mesh->m_material->setStiffness(50);
+	mesh->m_material->setDynamicFriction(0.8);
+	mesh->m_material->setStaticFriction(0.8);
 
-	return spring;
+	mesh->setLocalPos(0, 0, 0);
+
+	return mesh;
 }
 
 cMesh* createWall()
@@ -365,39 +386,30 @@ cMesh* createWall()
 	return wall;
 }
 
-void updateGraphics(void)
+void updateGraphics()
 {
-    world->updateShadowMaps(false, mirroredDisplay);
+	world->updateShadowMaps(false, mirroredDisplay);
 
-    camera->renderView(width, height);
+	camera->renderView(width, height);
 
-    glFinish();
+	glFinish();
 
-    // check for any OpenGL errors
-    GLenum err;
-    err = glGetError();
-    if (err != GL_NO_ERROR) cout << "Error:  %s\n" << gluErrorString(err);
+	// check for any OpenGL errors
+	GLenum err;
+	err = glGetError();
+	if (err != GL_NO_ERROR) cout << "Error:  %s\n" << gluErrorString(err);
 }
 
-void updateHaptics(void)
+void updateHaptics()
 {
-    simulationRunning  = true;
-    simulationFinished = false;
+	simulationRunning = true;
+	simulationFinished = false;
 
 	while (simulationRunning)
 	{
-		cVector3d pos;
-		hapticDevice->getPosition(pos);
-
-		cVector3d linearVelocity;
-		hapticDevice->getLinearVelocity(linearVelocity);
-
-		const auto indention = max(0.0, pos.y() + 0.085);
-		if (indention < 0.0)
-			spring->setLocalPos(0, 0, 0);
-		else
-			spring->setLocalPos(0, indention, 0);
+		slave->spin();
+		master->spin();
 	}
 
-    simulationFinished = true;
+	simulationFinished = true;
 }
