23d2481d558bd19cc4684b02a158ee7f12d999c8
bold <bold@cryptoguru.com>
2019-11-06 20:19:15 -0800

    fix rate limiter

--- examples/GLFW/32-spring/RateLimiter.cpp
+++ examples/GLFW/32-spring/RateLimiter.cpp
@@ -5,7 +5,7 @@ bool RateLimiter::limited()
 	const auto now = chrono::high_resolution_clock::now();
 	const auto lapse = chrono::duration_cast<std::chrono::microseconds>(now - m_last).count();
 	m_last = now;
-	m_tokens += double(lapse / 1e-6) * m_rate;
+	m_tokens += double(lapse * 1e-6) * m_rate;
 
 	if (m_tokens > m_rate)
 		m_tokens = m_rate;
